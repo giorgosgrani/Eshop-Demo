8a690d4431badf2d36cacad8bffcb341b3a1e11e
granisiotis <granisiotisg@gmail.com>
2026-06-22 13:02:45 +0300

    added button and add to cart

--- scripts/index.js
+++ scripts/index.js
@@ -1,4 +1,5 @@
-import { products } from "../data/products.js";
+import { products, cart } from "../data/products.js";
+
 
 let productsHTML = "";
 
@@ -35,14 +36,12 @@ products.forEach((product)=>{
               <option value="9">9</option>
               <option value="10">10</option>
             </select>
+            <div class="added-to-cart added-to-cart-inactive">
+            <img class="added-icon" src="../images/added-to-cart.svg">
+          </div>
           </div>
 
           <div class="product-spacer"></div>
-
-          <div class="added-to-cart">
-            <img src="images/icons/checkmark.png">
-            Added
-          </div>
 
           <button class="add-to-cart-button button-primary js-add-to-cart" data-product-id="${product.id}">
             Add to Cart
@@ -52,4 +51,43 @@ products.forEach((product)=>{
 });
 
 document.querySelector('.products-grid').innerHTML= productsHTML;
-console.log(products);
+
+document.querySelectorAll('.js-add-to-cart').forEach((button) => {
+  button.addEventListener('click', () => {
+    const productID = button.dataset.productId;
+    let matchingItem;
+
+    cart.forEach((item) => {
+      if (productID === item.productId) {
+        matchingItem = item;
+      }
+    });
+
+    if (matchingItem) {
+      matchingItem.quantity += 1;
+    } else {
+      cart.push({
+        productId: productID,
+        quantity: 1
+      });
+    }
+
+    let cartQuantity = 0;
+    cart.forEach((item) => {
+      cartQuantity += item.quantity;
+    });
+
+    document.querySelector('.js-cart-quantity').innerHTML = cartQuantity;
+
+    const productContainer = button.closest('.product-container');
+    const addedMessage = productContainer.querySelector('.added-to-cart');
+
+    addedMessage.classList.remove('added-to-cart-inactive');
+    addedMessage.classList.add('added-to-cart-active');
+
+    setTimeout(() => {
+      addedMessage.classList.remove('added-to-cart-active');
+      addedMessage.classList.add('added-to-cart-inactive');
+    }, 2000);
+  });
+});
